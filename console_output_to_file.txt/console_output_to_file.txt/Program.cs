2c36f684432010f8f678fe8342e472449f8137da
Vasubanhu <shangrila1721@gmail.com>
2021-01-18 22:41:39 +0500

    App saves data from console to file(.txt)

---
+++ console_output_to_file.txt/console_output_to_file.txt/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace appricot.test_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strs = new List<string>();
+            string path = @$"{DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss")}.txt";
+
+            Console.WriteLine("Enter text line and press Enter for new line. Press Ctrl + S to Save file.\n");
+
+            do
+            {
+                ConsoleKeyInfo button = Console.ReadKey(true);
+
+                //newline
+                if (button.Key == ConsoleKey.Enter)
+                    Console.WriteLine();
+
+                //save to textfile
+                if (button.Modifiers == ConsoleModifiers.Control && button.Key == ConsoleKey.S)
+                {
+                    FileInfo fileInf = new FileInfo(path);
+                    StreamWriter sw = new StreamWriter(path, false);
+                    sw.AutoFlush = true;
+                    Console.SetOut(sw);
+                    strs.ForEach(s => Console.Out.WriteLine(s));
+                    Console.Out.Close();
+
+                    sw = new StreamWriter(Console.OpenStandardOutput());
+                    sw.AutoFlush = true;
+                    Console.SetOut(sw);
+
+                    if (fileInf.Exists)
+                    {
+                        Console.Out.WriteLine($"\nFile successfully saved. {fileInf.Length} bytes.\n");
+                    }
+
+                    continue;
+                }
+
+                string str = Console.ReadLine();
+                strs.Add(str);
+
+            } while (true);
+        }
+    }
+}
